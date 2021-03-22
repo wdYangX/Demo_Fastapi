ecf5bec7d67a99d2ee01b17418a8b018514ea42d
duongpd <duongpd@bap.jp>
2021-03-22 21:23:32 +0700

    Update app

--- src/core/config.py
+++ src/core/config.py
@@ -5,7 +5,7 @@ from pydantic import AnyHttpUrl, BaseSettings, HttpUrl, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
-    API_V1_STR: str = "/api/v1"
+    API_V1_STR: str = "/apis/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
